faeec2ab51bb3bb69dfc0e58817bdac5b30a2dcd
Nikolay Bolotnikov <jokernix@mail.ru>
2017-11-30 19:46:03 +0300

    updated methods for profile component

--- src/components/user/user.controller.js
+++ src/components/user/user.controller.js
@@ -1,24 +1,59 @@
+const { asyncWrap } = require('../../../utils');
 const service = require('./user.service');
 
-let get = async (req, res, next) => {
-    res.send('get user');
+let get = (req, res, next) => {
+    res.json({
+        status: 'success',
+        data: req.user
+    });
 };
 
-let update = async (req, res, next) => {
-    res.send('update user');
+let update = async(req, res, next) => {
+
+    if (!req.body.email) throwError();
+
+    let savedUser = await service.update(req.user.get('_id'), {
+        email: req.body.email
+    });
+
+    res.json({
+        status: 'success',
+        data: savedUser
+    });
 };
 
-let remove = async (req, res, next) => {
-    res.send('remove user');
+let remove = async(req, res, next) => {
+
+    await service.update(req.user.get('_id'), {
+        deleted: true
+    });
+
+    res.json({
+        status: 'success',
+        data: null
+    });
 };
 
-let changePassword = async (req, res, next) => {
+let changePassword = async(req, res, next) => {
+    let user = req.user;
 
+    let tokens = await service.changePassword(user, req.body);
+    
+    res.json({
+        status: 'success',
+        data: tokens
+    });
 };
 
 module.exports = {
-    get,
-    update,
-    remove,
-    changePassword
+    get: get,
+    update: asyncWrap(update),
+    remove: asyncWrap(remove),
+    changePassword: asyncWrap(changePassword)
 };
+
+function throwError(message = 'Invalid data. Try again') {
+    let err = new Error(message);
+    err.status = 400;
+    throw err;
+}
--- src/components/user/user.service.js
+++ src/components/user/user.service.js
@@ -11,3 +11,26 @@ module.exports.get = async id => {
 
     return user;
 };
+
+module.exports.update = async(id, fields) => {
+    let options = {
+        new: true
+    };
+    let updatedUser = await User.findByIdAndUpdate(id, fields, options)
+        .exec();
+    return updatedUser;
+}
+
+module.exports.changePassword = async(user, {newPassword, oldPassword}) => {
+    let isMatch = await user.comparePassword(oldPassword);
+    if (!isMatch) {
+        let err = new Error('Invalid password. Try again');
+        err.status = 400;
+        throw err;
+    }
+
+    user.password = newPassword;
+    let savedUser = await user.save();
+
+    return savedUser.getToken();
+}
