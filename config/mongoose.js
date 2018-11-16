20a882f7d56c2bc8be3e384a601aa9333fea4957
Nikolay Bolotnikov <jokernix@mail.ru>
2018-11-16 13:33:51 +0300

    updated libs, and fixed vulnerabilities

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 const Promise = require('bluebird');
 const config = require('./config');
 
-mongoose.connect(config.mongodb.uri);
+mongoose.connect(config.mongodb.uri, { useCreateIndex: true, useNewUrlParser: true });
 
 mongoose.Promise = Promise;
